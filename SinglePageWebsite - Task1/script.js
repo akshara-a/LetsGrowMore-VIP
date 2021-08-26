f1589b47e8c60f099bb996518c044eef44661922
akshara-a <68069921+akshara-a@users.noreply.github.com>
2021-08-26 20:47:42 +0530

    Add files via upload

---
+++ SinglePageWebsite - Task1/script.js
@@ -0,0 +1,9 @@
+// Navigation Bar
+function ToggleFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}
